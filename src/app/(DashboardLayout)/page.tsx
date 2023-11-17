3b0bfd6c33bac0c3745e76949d7e046f37eb33b9
Mayank-2022 <103130933+Mayank-2022@users.noreply.github.com>
2023-11-17 22:52:23 +0530

    Dashboard

---
+++ src/app/(DashboardLayout)/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { Grid, Box } from '@mui/material';
+import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
+
+const Dashboard = () => {
+  return (
+    <PageContainer title="Dashboard" description="this is Dashboard">
+    <Box mt={3}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={12}>
+        </Grid>
+        {/* ------------------------- row 1 ------------------------- */}
+        <Grid item xs={12} lg={4}>
+        </Grid>
+        <Grid item xs={12} lg={8}>
+        </Grid>
+        <Grid item xs={12} lg={12}>
+        </Grid>
+      </Grid>
+    </Box>
+  </PageContainer>
+  )
+}
+
+export default Dashboard;
